bc5decafe2f205cd746432226a5ce591fdb64a4c
takahiro-yonei <yonet77@gmail.com>
2009-10-14 01:23:14 +0900

    2009/10/14: ユーザ管理用画面を追加
    Signed-off-by: takahiro-yonei <yonet77@gmail.com>

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -13,7 +13,37 @@ class AdminController < ApplicationController
     puts 'user_index!'
     puts '--------------------------------'
 		
-		@Users = User.find(:all)
+		@users = User.find(:all)
+	end
+	
+	def user_edit
+    puts '--------------------------------'
+    puts 'user_edit!'
+    puts '--------------------------------'
 		
+		@user = User.find(params[:id])
+	end
+	
+	def user_save
+    puts '--------------------------------'
+    puts 'user_save!'
+    puts '--------------------------------'
+		
+		p params[:user]
+		@user = User.find(params[:id])
+		@user.update_attributes(params[:user])
+		
+		redirect_to url_for(:controller=>:admin, :action=>:user_index)
+	end
+	
+	def user_delete
+    puts '--------------------------------'
+    puts 'user_delete!'
+    puts '--------------------------------'
+		
+		@user = User.find(params[:id])
+		@user.destroy
+		
+		redirect_to url_for(:controller=>:admin, :action=>:user_index)
 	end
 end
